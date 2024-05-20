ab15fe0692190e9e3a8640e8904c7337a2aa96b5
DavidePrelati <davide.prelati@gmail.com>
2024-05-20 15:13:07 +0200

    add

---
+++ src/Esercizio_1.java
@@ -0,0 +1,13 @@
+public class Esercizio_1 {
+    public static void main(String[] args) {
+        int result = prod(20,3);
+        System.out.println("il risultato è " +result);
+    }
+
+    public static int prod(int n1, int n2){
+        return n1*n2;
+    }
+
+
+
+}
